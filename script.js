63718e25c9c5a5a7ba046047af4674ec63ecf8a0
hide <hide85x@gmail.com>
2019-05-27 13:20:36 +0200

    correction for mobile devices

--- script.js
+++ script.js
@@ -1,68 +1,71 @@
-$(function(){
+$(function () {
 
-    const myOption= $('#option');
-    const myInput= $('input');
-    const button= $('#send');
-    const myResults= $('#results')
-    
-    console.log(myInput.val())
-    $('#refresh').click(function(){
-      location.reload();
-    })
-    
-    let searchOption= '';
-    let ApiUrl='https://swapi.co/api'
-    myOption.change(function(e){
-      searchOption= this.value
-      console.log(searchOption)
-    })
-    $('#input').keypress(function(e){
-      if (e.which===13){
-        $('#send').click()
-      }
-    })
-    
-    
-    button.click(function(e){
-    
-    const myValue= myInput.val();
-    const ApiToSend= ApiUrl+'/'+searchOption+'/?search='+ myValue;
-    console.log(ApiToSend) 
-        fetch(ApiToSend)
-        .then(data => {return data.json()})
-        .then((data) => { data.results
-            console.log(data.results)
-            
-                
-            
-            const generateHtml= (text) => '<li>' +text+ '</li>'
-            
-            const showResults=(searchOption, results)=> {
-                
-                let html;
-                
-                myResults.empty()
-               if(searchOption==='Choose...'){
-                    alert("a category choose must You havexxxx!")
-                } else if(searchOption==='people') {
-                    html= data.results.map(result=> 
-                        generateHtml('<b>Name : </b>' + result.name+'<b></br>Born : </b>'+ result.birth_year+  '</br> <b> Height: </b>' + result.height+ 'cm</br> <b> Mass: </b>'+ result.mass +' kg'))
-                
-                }else if(searchOption==='films'){
-                    html= data.results.map(result=>generateHtml('<b> Title : </b>' + result.title +' </br> <b> Plot  : </b>' + result.opening_crawl +''))
+  const myOption = $('#option');
+  const myInput = $('input');
+  const button = $('#send');
+  const myResults = $('#results')
 
-                }
-                if(data.results.length===0) {
-                    alert('found nothing have I...') }
-                
-                 myResults.prepend(html)
-                
-            }
-            showResults(searchOption, results)
-            $('li').addClass('active')
-        })
-        .catch(err=> alert("a category choose must You have!"))
-        
+  console.log(myInput.val())
+  $('#refresh').click(function () {
+    location.reload();
+  })
+
+  let searchOption = '';
+  let ApiUrl = 'https://swapi.co/api'
+  myOption.change(function (e) {
+    searchOption = this.value
+    console.log(searchOption)
+  })
+  $('#input').keypress(function (e) {
+    if (e.which === 13) {
+      $('#send').click()
+    }
+  })
+
+
+  button.click(function (e) {
+
+    const myValue = myInput.val();
+    const ApiToSend = ApiUrl + '/' + searchOption + '/?search=' + myValue;
+    console.log(ApiToSend)
+    fetch(ApiToSend)
+      .then(data => { return data.json() })
+      .then((data) => {
+        data.results
+        console.log(data.results)
+
+
+
+        const generateHtml = (text) => '<li>' + text + '</li>'
+
+        const showResults = (searchOption, results) => {
+
+          let html;
+
+          myResults.empty()
+          if (searchOption === 'Choose...') {
+            alert("a category choose must You have!")
+          } else if (searchOption === 'people') {
+            html = data.results.map(result =>
+              generateHtml('<b>Name : </b>' + result.name + '<b></br>Born : </b>' + result.birth_year + '</br> <b> Height: </b>' + result.height + 'cm</br> <b> Mass: </b>' + result.mass + ' kg'))
+
+          } else if (searchOption === 'films') {
+            html = data.results.map(result => generateHtml('<b> Title : </b>' + result.title + ' </br> <b> Plot  : </b>' + result.opening_crawl + ''))
+
+          }
+          if (data.results.length === 0) {
+            alert('found nothing have I...')
+          }
+
+          myResults.prepend(html)
+
+        }
+        showResults(searchOption, results)
+        $('.wyniki').addClass('active')
+
+      })
+      .catch(err => alert("a category choose must You have!"))
+
 
 
 
